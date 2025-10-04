ce4d8ddc8806569e8cf902f1d8a1666959234478
lync <linkrodasfox@gmail.com>
2025-10-04 11:39:37 -0400

    replace with full working bundle

---
+++ public/js/firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+window.firebaseConfig = {
+  apiKey: "AIzaSyCTahUWyuA4-lGPwJqMMHRENJagFr6dIBg",
+  authDomain: "lynctree-8e976.firebaseapp.com",
+  projectId: "lynctree-8e976",
+  storageBucket: "lynctree-8e976.firebasestorage.app",
+  messagingSenderId: "536327079303",
+  appId: "1:536327079303:web:73a9cf88c58fe8fc4e3f22"
+};
